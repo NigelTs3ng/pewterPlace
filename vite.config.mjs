f6b18dcd97909aff97938918e382fea9074ac7f1
NigelTs3ng <your.email@example.com>
2025-09-11 15:38:40 +0800

    push test2

--- vite.config.mjs
+++ vite.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
   },
   plugins: [tsconfigPaths(), react(), tagger()],
   server: {
-    port: "4028",
+    port: "2000",
     host: "0.0.0.0",
     strictPort: true,
     allowedHosts: ['.amazonaws.com', '.builtwithrocket.new']
